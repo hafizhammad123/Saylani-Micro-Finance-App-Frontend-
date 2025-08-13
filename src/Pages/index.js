6cc3e5ef615de889aa2b67026fd5c0462a70b60a
Muhammad Hammad <hafizhammadmemon38@gmail.com>
2025-08-13 15:43:18 +0500

    some changes

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -7,13 +7,14 @@ import NewPassword from "./Auth/NewPassword";
 import LonaRequest from "./LoanReqForm/LonaRequest";
 import UserDashboard from "./Dashboard/UserDashboard";
 import AdminDashbord from "./Admin/AdminDashbord";
-import AllRequestDetail from "./Admin/allRequestDetail";
+import AllRequestDetail from "./Admin/AllRequestDetail.jsx";
 import AppointmentModal from "./Admin/AppointmentModal";
 import UserLoanReq from "./UserLoanReqPage/UserLoanReq";
 import AppointmentSlip from "./ApointmentSlip/AppointmentSlip";
-import VeiwAppointSlips from "./Admin/veiwAppointSlips";
+import VeiwAppointSlips from "./Admin/VeiwAppointSlips.jsx";
 
 export {
-    LandingPage, ContactPage, Calculatepage, Register, Login, NewPassword, LonaRequest, UserDashboard, AdminDashbord, AllRequestDetail
-    , AppointmentModal, UserLoanReq, AppointmentSlip,VeiwAppointSlips
+    LandingPage, ContactPage, Calculatepage, Register, Login, NewPassword, LonaRequest, UserDashboard, AdminDashbord,
+    AllRequestDetail
+    , AppointmentModal, UserLoanReq, AppointmentSlip, VeiwAppointSlips
 }
